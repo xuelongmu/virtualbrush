a66c34aab4b9737509691b2c9451699861eaebff
xuelongmu <xuelongmu@gmail.com>
2020-04-17 20:16:46 -0400

    added default UE files

---
+++ Source/VirtualBrush/VirtualBrushGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "VirtualBrushGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class VIRTUALBRUSH_API AVirtualBrushGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
