c4b964280a1b008735915308ca393f9f033eb720
xuelongmu <xuelongmu@gmail.com>
2020-05-03 12:54:43 -0400

    finished lecture 49

--- Source/VirtualBrush/UIPointerHandController.h
+++ Source/VirtualBrush/UIPointerHandController.h
@@ -2,8 +2,10 @@
 
 #pragma once
 
+#include "Components/WidgetInteractionComponent.h"
 #include "CoreMinimal.h"
 #include "HandControllerBase.h"
+
 #include "UIPointerHandController.generated.h"
 
 /**
@@ -13,5 +15,15 @@ UCLASS()
 class VIRTUALBRUSH_API AUIPointerHandController : public AHandControllerBase
 {
 	GENERATED_BODY()
-	
+
+public:
+	// Sets default values for this actor's properties
+	AUIPointerHandController();
+	void TriggerPressed() override;
+	void TriggerReleased() override;
+
+private:
+	//components
+	UPROPERTY(VisibleAnywhere)
+	UWidgetInteractionComponent* Pointer;
 };
